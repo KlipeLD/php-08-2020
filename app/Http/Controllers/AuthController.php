99c9c24fea5c9bc38bf021494705347c6986554f
KLIPE_LD <klipe.ld@gmail.com>
2023-01-24 20:59:55 +0100

    sss
    s

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use app\Models\User;
+use App\Models\User;
 use App\Http\Requests\Auth\LoginRequest;
 
 class AuthController extends Controller
